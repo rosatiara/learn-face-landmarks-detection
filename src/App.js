4435f7e6ad1e04778c457a122bba9fda532703db
rosa <rosatiaragaluh@gmail.com>
2022-01-27 20:17:55 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+/*
+TODO:
+1. Install dependencies
+2. Import dependencies
+3. Setup webcam and canvas
+4. Define references to those
+5. Load facemesh
+6. Detect function
+7. Drawing utilities
+8. Load triangulation
+9. Setup triangle path
+10. Setup point drawing
+11. Add drawMesh to detect function
+*/
+
+
 import logo from './logo.svg';
 import './App.css';
 
